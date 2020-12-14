77b889eb89d92fb1b366ea4ee46032f7091524d0
Victor dey <victordey2007@gmail.com>
2020-12-14 21:52:56 +0000

    added doc and added md file

--- models/stg_payments.sql
+++ models/stg_payments.sql
@@ -8,6 +8,6 @@ source as (
         coalesce(amount,0) / 100 as amount, -- converting from cents to dollars
         created as created_at,
         _batched_at
-    from raw.stripe.payment
+    from {{ source('stripe', 'payment') }}
 )
 select * from source
